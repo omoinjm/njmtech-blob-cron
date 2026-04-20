3e46bf1851575f168878b5f6fe11caab2e9e241e
Nhlanhla Malaza <njmcloud@gmail.com>
2026-04-20 11:40:24 +0200

    gucci

---
+++ mocks/dotenv.py
@@ -0,0 +1,2 @@
+def load_dotenv(*args, **kwargs): pass
+def find_dotenv(*args, **kwargs): pass
---
+++ mocks/vercel_blob.py
@@ -0,0 +1,3 @@
+def list(*args, **kwargs): pass
+def download(*args, **kwargs): pass
+def put(*args, **kwargs): pass
